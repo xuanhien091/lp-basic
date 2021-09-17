0e99ca03bc9232f946671e09b12e218e3bfcb865
スアンヒエン <hien@ksb-sendai.co.jp>
2021-09-17 10:19:44 +0900

    feat: fix blank

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+const sass = require('gulp-sass');
 var minCss = require("gulp-minify-css");
 var rename = require("gulp-rename");
-
+var cleanCSS = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
 var gulpIf = require("gulp-if");
-
+var strip = require('gulp-strip-comments');
 var imagemin = require("gulp-imagemin");
 var del = require("del");
 var cache = require("gulp-cache");
@@ -23,11 +23,37 @@ var fileInclude = require("gulp-file-include");
 var autoprefixer = require("gulp-autoprefixer");
 var babel = require("gulp-babel");
 var htmlhint = require("gulp-htmlhint");
+const fibers = require('fibers');
+const postcss = require('gulp-postcss');
+sass.compiler = require('sass');
 
 var options = minimist(process.argv.slice(2)); // コマンドライン・オプション読み込み
 var isProduction = options.env == "production"; // --env=productionと指定されたらリリース用
 var parentTaskName = process.argv[2] || "default"; // 指定されたタスク名
 var isWatch = parentTaskName == "watch"; // watchタスクで起動されたか
+
+const paths = {
+  html: {
+    src: 'sources/modules/pages/**/*.html',
+    dest: 'public_html/'
+  },
+  css: {
+    src: 'sources/assets/sass/**/*.scss',
+    dest: 'public_html/css/'
+  },
+  js: {
+    src: 'sources/assets/js/*.js',
+    dest: 'public_html/js/'
+  },
+  images: {
+    src: 'sources/assets/images/**/*.+(png|jpg|gif|svg)',
+    dest: 'public_html/images'
+  }, 
+  fonts: {
+    src: 'sources/assets/fonts/**/*',
+    dest: 'public_html/fonts'
+  }
+}
 
 gulp.task("browserSync", function (done) {
   browserSync.init({
@@ -47,58 +73,82 @@ gulp.task("browserSync", function (done) {
   });
   done();
 });
-
+//---------------------------------------------------
+// HTML
+//---------------------------------------------------
 gulp.task("file-include", function (done) {
   gulp
-    .src(["sources/modules/pages/**/*.html"])
+    .src([paths.html.src])
     .pipe(
       fileInclude({
         prefix: "@@",
         basepath: "@file",
       })
     )
-    .pipe(gulp.dest("public_html/"));
+    .pipe(gulp.dest(paths.html.dest));
 
   done();
 });
-
-//js minify
-gulp.task("js", function (done) {
-  gulp
-    .src("sources/assets/js/**/*.js")
-    .pipe(gulpIf(isWatch, plumber())) // watchタスクの場合、エラーが発生しても無視
-    .pipe(gulpIf(!isProduction, sourcemaps.init())) // 本番以外はソースマップ初期化
-    .pipe(
-      babel({
-        presets: ["@babel/preset-env"],
-      })
-    )
-    .pipe(concat("all.js")) // Javascriptを結合
-    .pipe(gulpIf(isProduction, stripDebug())) // 本番でははconsole.logを除去
-    .pipe(uglify()) // all.jsを最小化
-    .pipe(rename("all.min.js")) // all-min.jsにリネーム
-    .pipe(gulpIf(!isProduction, sourcemaps.write("./"))) // 本番以外はソースマップ出力
-    .pipe(gulp.dest("public_html/js/"));
+//---------------------------------------------------
+// JAVASCRIPT
+//---------------------------------------------------
+gulp.task('js', function(done){
+   gulp.src(paths.js.src)
+  .pipe(concat('all.js'))
+  .pipe(uglify()) 
+  .pipe(rename("all.min.js"))
+  .pipe(gulp.dest(paths.js.dest));
   done();
+})
+//---------------------------------------------------
+// CSS
+//---------------------------------------------------
+gulp.task('sass', () => {
+  return gulp.src(paths.css.src)
+    .pipe(sass({
+      fiber: fibers,
+      style: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      require('autoprefixer')({
+        grid: "autoplace",
+        cascade: false
+      }),
+      require('css-mqpacker')
+    ]))
+    .pipe(strip())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest(paths.css.dest))
+});
+//---------------------------------------------------
+// IMAGES
+//---------------------------------------------------
+gulp.task("images", function () {
+  return (
+    gulp
+      .src(paths.images.src)
+      .pipe(
+        cache(
+          imagemin({
+            interlaced: true,
+          })
+        )
+      )
+      .pipe(gulp.dest(paths.images.dest))
+  );
 });
 
-gulp.task("build-css", function () {
+gulp.task("fonts", function () {
   return gulp
-    .src("sources/assets/sass/**/*.scss")
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("public_html/css/"))
-    .pipe(minCss())
-    .pipe(rename({ extname: ".min.css" }))
-    .pipe(gulp.dest("public_html/css/"));
+    .src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
 });
 
 gulp.task("watch", function (done) {
-  gulp.watch("sources/assets/sass/**/*.scss", gulp.task("build-css"));
+  gulp.watch("sources/assets/sass/**/*.scss", gulp.task("sass"));
   gulp.watch("sources/assets/fonts/*", gulp.task("fonts"));
   gulp.watch("sources/assets/images/*", gulp.task("images"));
   gulp.watch("sources/assets/js/**/*.js", gulp.task("js"));
@@ -109,27 +159,6 @@ gulp.task("watch", function (done) {
 
 gulp.task("reload", function () {
   gulp.watch("public_html/**/**/*.*", browserSync.reload);
-});
-
-gulp.task("images", function () {
-  return (
-    gulp
-      .src("sources/assets/images/**/*.+(png|jpg|gif|svg)")
-      .pipe(
-        cache(
-          imagemin({
-            interlaced: true,
-          })
-        )
-      )
-      .pipe(gulp.dest("public_html/images"))
-  );
-});
-
-gulp.task("fonts", function () {
-  return gulp
-    .src("sources/assets/fonts/**/*")
-    .pipe(gulp.dest("public_html/fonts"));
 });
 
 gulp.task("clean:public_html", function (done) {
@@ -145,24 +174,24 @@ gulp.task("cache:clear", function (callback) {
   return cache.clearAll(callback);
 });
 
-gulp.task("html", function () {
-  return gulp
-    .src("sources/modules/components/**/*.html")
-    .pipe(
-      htmlhint({
-        "doctype-first": false,
-        "tagname-lowercase": true,
-        "attr-no-duplication": true,
-        "attr-value-double-quotes": true,
-        "alt-require": true,
-        "id-unique": true,
-        "id-class-value": false,
-        "src-not-empty": true,
-        "inline-style-disabled": true,
-      })
-    )
-    .pipe(htmlhint.reporter());
-});
+// gulp.task("html", function () {
+//   return gulp
+//     .src("sources/modules/components/**/*.html")
+//     .pipe(
+//       htmlhint({
+//         "doctype-first": false,
+//         "tagname-lowercase": true,
+//         "attr-no-duplication": true,
+//         "attr-value-double-quotes": true,
+//         "alt-require": true,
+//         "id-unique": true,
+//         "id-class-value": false,
+//         "src-not-empty": true,
+//         "inline-style-disabled": true,
+//       })
+//     )
+//     .pipe(htmlhint.reporter());
+// });
 
 // Build Sequences
 // ---------------
@@ -170,7 +199,7 @@ gulp.task(
   "default",
   gulp.series(
     "clean:public_html",
-    "build-css",
+    "sass",
     "js",
     "file-include",
     "images",
@@ -188,7 +217,7 @@ gulp.task(
   "build",
   gulp.series(
     "clean:public_html",
-    "build-css",
+    "sass",
     "js",
     "file-include",
     "images",
