4d93f030f4b422abda983ea600c02ea40507d2c1
沼倉琢 <umakura@ksb-sendai.co.jp>
2022-05-17 16:15:20 +0900

    feat: header create pc

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 500,
   speedAsDuration: true,
 });
+
+// navigation menu
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+const navClose = document.querySelector(".nav-menu__text--close");
+
+hamburgerMenu.addEventListener("click", () => {
+  if (navMenu.classList.contains("slide-in") !== true) {
+    navMenu.classList.add("slide-in");
+  }
+  navMenu.classList.replace("slide-out", "slide-in");
+});
+
+navClose.addEventListener("click", () => {
+  navMenu.classList.replace("slide-in", "slide-out");
+});
